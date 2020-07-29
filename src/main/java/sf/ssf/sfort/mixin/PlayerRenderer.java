26c77eb184e17fc915b9c927c0cf5ae6af76bb78
SFort <github@ssf.tf>
2020-07-29 15:03:45 +0200

    v1.0.1 slight performance improvment

--- src/main/java/sf/ssf/sfort/mixin/PlayerRenderer.java
+++ src/main/java/sf/ssf/sfort/mixin/PlayerRenderer.java
@@ -2,8 +2,10 @@ package sf.ssf.sfort.mixin;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,12 +15,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Environment(EnvType.CLIENT)
-@Mixin(EntityRenderer.class)
+@Mixin(PlayerEntityRenderer.class)
 public class PlayerRenderer {
 	@Inject(method = "renderLabelIfPresent", at = @At("HEAD"), cancellable = true)
-	public void render(Entity entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
-		if (entity instanceof PlayerEntity){
-			info.cancel();
-		}
+	public void render(AbstractClientPlayerEntity abstractClientPlayerEntity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo info) {
+		info.cancel();
 	}
 }
